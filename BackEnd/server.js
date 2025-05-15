027352dd627244fef92bd390ff613464db3677a9
ayush saini <ayushsaini16703@gmail.com>
2025-05-16 01:54:05 +0530

    first commit

---
+++ BackEnd/server.js
@@ -0,0 +1,8 @@
+const app = require('./src/app')
+const dotenv = require('dotenv')
+dotenv.config();
+
+app.listen(3000,()=>{
+    console.log('server is running on http://localhost:3000');
+    
+})
